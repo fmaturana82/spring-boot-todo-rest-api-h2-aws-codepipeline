2b152999a3ead8e31ac94a1f135d8e1a3347c963
fm <fm@gmail.com>
2022-12-26 20:02:29 -0300

    3 commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World 2023 ***";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
