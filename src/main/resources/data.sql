6b271d555184763c23fb6412172dc1676a3ca60c
fm <fm@gmail.com>
2022-12-26 19:45:55 -0300

    second commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,5 +6,3 @@ values(10002, 'in28minutes', 'Learn Data JPA', sysdate(), false);
 
 insert into todo(id, username,description,target_date,is_done)
 values(10003, 'in28minutes', 'Learn Microservices', sysdate(), false);
-
-data.sql
